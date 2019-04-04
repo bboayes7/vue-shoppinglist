75100c4fce12036f6916c1e3640c187d1699ae2a
Barry <barryboayes17@gmail.com>
2019-04-04 13:54:53 -0700

    it works

--- js/app.js
+++ js/app.js
@@ -1,23 +1,26 @@
 var shoppingList = new Vue({
     el: '#shopping-list',
     data: {
-        state: 'default',
         header: 'Shopping list app',
         newItem: '',
         items: [
             {
+                id: 0,
                 label: '10 party hats',
                 purchased: true
             },
             {
+                id: 1,
                 label: '2 board games',
                 purchased: false
             },
             {
+                id: 2,
                 label: '20 cups',
                 purchased: true
             }
         ],
+        messageArchive: 'archive this item'
     },
     computed: {
         reversedItems() {
@@ -29,18 +32,26 @@ var shoppingList = new Vue({
             if(!this.newItem){
                 return;
             }
+            
             this.items.push({
+                id: this.items.length,
                 label: this.newItem,
                 purchased: false
             });
             this.newItem = '';
         },
-        changeState: function(newState){
-            this.state = newState;
-            this.newItem = '';
-        },
         togglePurchased: function(item){
             item.purchased = !item.purchased;
+        },
+        deleteItem: function(deletedItem){
+            this.items = this.items.filter(item => item !== deletedItem)
+        },
+        checkboxToggled(item){
+            if(item.purchased){
+                return 'far fa-check-square'
+            } else{
+                return 'far fa-square'
+            }
         }
     }
 })
