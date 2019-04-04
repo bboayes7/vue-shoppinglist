831998e84b917dd360e0ddfabceab06cc1df8916
Barry <barryboayes17@gmail.com>
2019-04-03 19:43:14 -0700

    app is functional but ugly

--- js/app.js
+++ js/app.js
@@ -1,21 +1,46 @@
 var shoppingList = new Vue({
     el: '#shopping-list',
     data: {
+        state: 'default',
         header: 'Shopping list app',
         newItem: '',
         items: [
-            '10 party hats',
-            '2 board games',
-            '20 cups',
+            {
+                label: '10 party hats',
+                purchased: true
+            },
+            {
+                label: '2 board games',
+                purchased: false
+            },
+            {
+                label: '20 cups',
+                purchased: true
+            }
         ],
+    },
+    computed: {
+        reversedItems() {
+            return this.items.slice(0).reverse();
+        }
     },
     methods:{
         addItem: function(){
-            if(!this.newItem)
-                return
-            
-            this.items.push(this.newItem);
+            if(!this.newItem){
+                return;
+            }
+            this.items.push({
+                label: this.newItem,
+                purchased: false
+            });
             this.newItem = '';
+        },
+        changeState: function(newState){
+            this.state = newState;
+            this.newItem = '';
+        },
+        togglePurchased: function(item){
+            item.purchased = !item.purchased;
         }
     }
 })
